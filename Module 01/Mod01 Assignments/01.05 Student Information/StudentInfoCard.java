b58dfa2fffce63a4c76c6a3629028abe885cba21
LFV1220 <51392988+LFV1220@users.noreply.github.com>
2020-11-11 10:03:04 -0500

    Add files via upload

---
+++ Module 01/Mod01 Assignments/01.05 Student Information/StudentInfoCard.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class StudentInfoCard here. Write a program to print information that would be found on a typical Student Information Card.
+ *
+ * @author Luis Vega
+ * @version 7/30/19
+ */
+public class StudentInfoCard
+{
+   public static void main(String [] args)
+   {
+       System.out.println("***********************************************");
+       System.out.println(" *                                             *");
+       System.out.println(" *              Student Information            *");
+       System.out.println(" *                                             *");
+       System.out.println(" ***********************************************");
+       
+       System.out.print("Name: Luis Vega");
+       System.out.println("              Go-by name: Frankie");
+       System.out.print("Birthday: 12/20/2001");
+       System.out.println("         Age: 17");
+       System.out.println("School: Nature Coast Technical High School");
+       System.out.println("City: Brooksville");
+       System.out.print("Cell phone: (954)880-5576");
+       System.out.println("    Parent Cell phone: (352)346-4855");
+       System.out.println("Math course for coming school year: College Algebra / Pre-Calc");
+       System.out.println("Previous experience: I learned a bit of Swift using xcode");
+       System.out.println("I am taking this course because I am interested in computer science and plan on taking this in college and having a career in this IT field");
+       
+    }
+}
---
+++ Module 01/Mod01 Lessons/01.05 HellowWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author Frankie
+ * @version v1
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello, Virtual World!");
+        System.out.println();
+        System.out.println("It is a great day for programming.");
+    }//end of main method
+  
+}//end of class
